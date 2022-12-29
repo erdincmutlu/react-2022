afbe10ded9c7364a8386f9a35443046638f2e047
Erdinc Mutlu <erdincmutlu@gmail.com>
2022-12-29 18:25:50 +0000

    Lesson 14

--- lesson_13b/index.js
+++ lesson_13b/index.js
@@ -1,3 +1,14 @@
+/**
+Challenge: 
+
+Part 1: Create a page of your own using a custom Page component
+
+It should return an ordered list with the reasons why you're
+excited to be learning React :)
+
+Render your list to the page
+ */
+
 function Page() {
   return (
     <ol>
