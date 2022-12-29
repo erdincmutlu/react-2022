cd7f00767c75717636be77a0dd73565828450716
Erdinc Mutlu <erdincmutlu@gmail.com>
2022-12-29 19:46:25 +0000

    Lesson 18

---
+++ lesson_18/index.js
@@ -0,0 +1,33 @@
+import Header from "./Header"
+
+function Footer() {
+  return (
+    <footer>
+      <small>© 2022 Mutlu development. All rights reserved.</small>
+    </footer>
+  )
+}
+
+function MainContent() {
+  return (
+    <div>
+      <h1>Reasons I'm excited to learn React</h1>
+      <ol>
+        <li>It's a popular library, so I'll be able to fit in with the cool kids!</li>
+        <li>I'm more likely to get a job as a developer if I know React</li>
+      </ol>
+    </div>
+  )
+}
+
+function Page() {
+  return (
+    <div>
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>
+  )
+}
+
+ReactDOM.render(<Page />, document.getElementById("root"));
