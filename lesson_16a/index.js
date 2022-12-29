814d3fa9c15456094fc5fa9dc6d02954c729ff98
Erdinc Mutlu <erdincmutlu@gmail.com>
2022-12-29 18:53:26 +0000

    Lesson 16

---
+++ lesson_16a/index.js
@@ -0,0 +1,47 @@
+/**
+Mini Challenge:
+Move the `header` element from Page into 
+its own component called "Header"
+*/
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <img src="./react-logo.png" width="40px" />
+      </nav>
+    </header>
+  )
+}
+
+function Footer() {
+  return (
+    <footer>
+      <small>© 2022 Mutlu development. All rights reserved.</small>
+    </footer>
+  )
+}
+
+function MainContent() {
+  return (
+    <div>
+      <h1>Reasons I'm excited to learn React</h1>
+      <ol>
+        <li>It's a popular library, so I'll be able to fit in with the cool kids!</li>
+        <li>I'm more likely to get a job as a developer if I know React</li>
+      </ol>
+    </div>
+  )
+}
+
+function Page() {
+  return (
+    <div>
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>
+  )
+}
+
+ReactDOM.render(<Page />, document.getElementById("root"));
---
+++ lesson_16b/index.js
@@ -0,0 +1,33 @@
+/**
+Mini Challenge:
+Move the `header` element from Page into 
+its own component called "Header"
+*/
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <img src="./react-logo.png" width="40px"/>
+      </nav>
+    </header>  
+  )
+}
+
+function Page() {
+  return (
+    <div>
+      <Header />
+      <h1>Reasons I'm excited to learn React</h1>
+      <ol>
+        <li>It's a popular library, so I'll be able to fit in with the cool kids!</li>
+        <li>I'm more likely to get a job as a developer if I know React</li>
+      </ol>
+      <footer>
+        <small>© 2022 Mutlu development. All rights reserved.</small>
+      </footer>
+    </div>
+  )
+}
+
+ReactDOM.render(<Page />, document.getElementById("root"));
