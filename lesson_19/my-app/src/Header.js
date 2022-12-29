406475a639ae25f5d95567d6463bc5c3f1d250f6
Erdinc Mutlu <erdincmutlu@gmail.com>
2022-12-29 21:04:21 +0000

    Lesson 19 local working

--- lesson_19/my-app/src/Header.js
+++ lesson_19/my-app/src/Header.js
@@ -1,10 +1,11 @@
 import React from "react"
+import logo from "./react-logo.png"
 
 export default function Header() {
     return (
         <header>
             <nav className="nav">
-                <img src="./react-logo.png" className="nav-logo" />
+                <img src={logo} className="nav-logo" />
                 <ul className="nav-items">
                     <li>Pricing</li>
                     <li>About</li>
--- lesson_19/my-app/src/index.js
+++ lesson_19/my-app/src/index.js
@@ -4,11 +4,7 @@ import ReactDOM from "react-dom"
 import Header from "./Header"
 import Footer from "./Footer"
 import MainContent from "./MainContent"
-
-/**
-Challenge: move the Footer and MainContent components
-into their own files.
-*/
+import "./index.css"
 
 function App() {
   return (
