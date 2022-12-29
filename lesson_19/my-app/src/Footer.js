3e82ef68ffaec2e388e3597e026fac53b1bc932e
Erdinc Mutlu <erdincmutlu@gmail.com>
2022-12-29 20:49:43 +0000

    Lesson 19

---
+++ lesson_19/my-app/src/Footer.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Footer() {
+    return (
+        <footer>
+            <small>© 2022 Mutlu development. All rights reserved.</small>
+        </footer>
+    )
+}
