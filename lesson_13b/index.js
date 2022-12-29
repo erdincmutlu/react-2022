250a3c273c57461f28956f790a22270f9de77d83
Erdinc Mutlu <erdincmutlu@gmail.com>
2022-12-29 18:13:24 +0000

    Lesson 13

---
+++ lesson_13b/index.js
@@ -0,0 +1,10 @@
+function Page() {
+  return (
+    <ol>
+      <li>It's a popular library, so I'll be able to fit in with the cool kids!</li>
+      <li>I'm more likely to get a job as a developer if I know React</li>
+    </ol>
+  )
+}
+
+ReactDOM.render(<Page />, document.getElementById("root"));
