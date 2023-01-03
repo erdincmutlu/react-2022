69927f03dfc8c799daa28e67e29fe07f02cf5c7b
Erdinc Mutlu <erdincmutlu@gmail.com>
2023-01-03 15:15:42 +0000

    Lesson s2-15

---
+++ lesson_s2_14/my-app/src/App.js
@@ -0,0 +1,21 @@
+import React from "react"
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Card from "./components/Card"
+
+export default function App() {
+    // <Hero />
+    return (
+        <div>
+            <Navbar />
+            <Card
+                img="katie-zaferes.png"
+                rating="5.0"
+                reviewCount={6}
+                country="USA"
+                title="Life Lessons with Katie Zaferes"
+                price={136}
+            />
+        </div>
+    )
+}
---
+++ lesson_s2_14/my-app/src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react"
+// import logo from "../images/airbnb-logo.png"
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img src="./images/airbnb-logo.png" className="nav--logo"/>
+        </nav>
+    )
+}
