aefc695d5bf7fae93eb2310fc779a95e638a60eb
Erdinc Mutlu <erdincmutlu@gmail.com>
2023-01-03 10:04:10 +0000

    Lesson s2-4

---
+++ lesson_s2_04/my-app/src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Card from "./components/Card"
+
+export default function App() {
+    // <Hero />
+    return (
+        <div>
+            <Navbar />
+            <Card />
+        </div>
+    )
+}
