2647013bbdc948603d6761b8c4fb9aa2938ffb45
Erdinc Mutlu <erdincmutlu@gmail.com>
2023-01-04 17:53:40 +0000

    Lesson s2-18

---
+++ lesson_s2_18/my-app/src/App.js
@@ -0,0 +1,16 @@
+import React from "react"
+import Joke from "./Joke"
+import jokesData from "./jokesData"
+
+function App() {
+    const jokeElements = jokesData.map(joke => {
+        return <Joke setup={joke.setup} punchline={joke.punchline} />
+    })
+    return (
+        <div>
+            {jokeElements}
+        </div>
+    )
+}
+
+export default App
