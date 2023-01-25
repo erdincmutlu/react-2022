3222cd13089db6c10af88be12756d3bfbfe8ffec
Erdinc Mutlu <erdincmutlu@gmail.com>
2023-01-25 21:54:00 +0000

    Lesson s3-22

---
+++ lesson_s3_22/my-app/src/App.js
@@ -0,0 +1,46 @@
+import React from "react"
+
+export default function App() {
+    const [contact, setContact] = React.useState({
+        firstName: "John",
+        lastName: "Doe",
+        phone: "+1 (719) 555-1212",
+        email: "itsmyrealname@example.com",
+        isFavorite: false
+    })
+    /**
+     * Challenge: Fill in the values in the markup
+     * using the properties of our state object above
+     * (Ignore `isFavorite` for now)
+     */
+    
+    let starIcon = contact.isFavorite ? "star-filled.png" : "star-empty.png"
+
+    function toggleFavorite() {
+        setContact(prevContact => ({
+            ...prevContact,
+            isFavorite: !prevContact.isFavorite
+        }))
+    }
+    
+    return (
+        <main>
+            <article className="card">
+                <img src="./images/user.png" className="card--image" />
+                <div className="card--info">
+                    <img 
+                        src={`../images/${starIcon}`} 
+                        className="card--favorite"
+                        onClick={toggleFavorite}
+                    />
+                    <h2 className="card--name">
+                        {contact.firstName} {contact.lastName}
+                    </h2>
+                    <p className="card--contact">{contact.phone}</p>
+                    <p className="card--contact">{contact.email}</p>
+                </div>
+                
+            </article>
+        </main>
+    )
+}
